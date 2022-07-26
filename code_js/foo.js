156efc4dceb5f8872674eaa5ef3d4d7652ca613a
kota <85015574+tiger-sea@users.noreply.github.com>
2022-07-26 12:10:06 +0900

    revised a little bit

--- code_js/foo.js
+++ code_js/foo.js
@@ -1,13 +1,34 @@
 let user_name;
 let year = new Date();
 
-user_name = prompt("Enter Your Name");
-if(user_name == "" || user_name == null) {
-  year_result = year.getFullYear();
-  user_name = "人間の" + year_result;
-  console.log(user_name);
-  document.getElementById("user_name").innerHTML = user_name;
+const keyName = 'visited';
+const keyValue = true;
+
+if (!sessionStorage.getItem(keyName)) {
+  //sessionStorageにキーと値を追加
+  sessionStorage.setItem(keyName, keyValue);
+
+  //ここに初回アクセス時の処理
+  user_name = prompt("Enter Your Name");
+  if(user_name == "" || user_name == null) {
+      year_result = year.getFullYear();
+      user_name = "人間の" + year_result;
+      console.log(user_name);
+      document.getElementById("user_name").innerHTML = user_name;
+  } else {
+      console.log(user_name);
+      document.getElementById("user_name").innerHTML = user_name;
+  }
+
 } else {
-  console.log(user_name);
-  document.getElementById("user_name").innerHTML = user_name;
+  //ここに通常アクセス時の処理
+  if(user_name == "" || user_name == null) {
+    year_result = year.getFullYear();
+    user_name = "人間の" + year_result;
+    console.log(user_name);
+    document.getElementById("user_name").innerHTML = user_name;
+  } else {
+    console.log(user_name);
+    document.getElementById("user_name").innerHTML = user_name;
+  }
 }
