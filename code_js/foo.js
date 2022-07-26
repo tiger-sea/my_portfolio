79070797682bb1d88f279079e31967aa39ae4874
kota <85015574+tiger-sea@users.noreply.github.com>
2022-07-27 02:19:45 +0900

    modified to fix name bugs and added photo

--- code_js/foo.js
+++ code_js/foo.js
@@ -1,34 +1,30 @@
+// https://into-the-program.com/execution-firsttime-access/
+// sessionStorageの参考サイト
 let user_name;
 let year = new Date();
 
 const keyName = 'visited';
-const keyValue = true;
 
-if (!sessionStorage.getItem(keyName)) {
-  //sessionStorageにキーと値を追加
-  sessionStorage.setItem(keyName, keyValue);
-
+if (sessionStorage.getItem(keyName) == null) {
   //ここに初回アクセス時の処理
   user_name = prompt("Enter Your Name");
+  //sessionStorageにキーと値を追加
+  sessionStorage.setItem(keyName, user_name);
   if(user_name == "" || user_name == null) {
       year_result = year.getFullYear();
       user_name = "人間の" + year_result;
-      console.log(user_name);
       document.getElementById("user_name").innerHTML = user_name;
   } else {
-      console.log(user_name);
       document.getElementById("user_name").innerHTML = user_name;
   }
-
 } else {
   //ここに通常アクセス時の処理
+  user_name = sessionStorage.getItem(keyName);
   if(user_name == "" || user_name == null) {
     year_result = year.getFullYear();
     user_name = "人間の" + year_result;
-    console.log(user_name);
     document.getElementById("user_name").innerHTML = user_name;
   } else {
-    console.log(user_name);
     document.getElementById("user_name").innerHTML = user_name;
   }
 }
