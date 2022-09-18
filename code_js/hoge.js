6145e04454eb5df12efa8db274764404bb9cade4
kota <85015574+tiger-sea@users.noreply.github.com>
2022-09-18 14:59:47 +0900

    fix: implementation of Zeller's congruence

--- code_js/hoge.js
+++ code_js/hoge.js
@@ -6,44 +6,44 @@ let m = date.getMonth() + 1;
 let d = date.getDate();
 let full = prompt(`Enter your birthday(e.g. ${y}/${m}/${d}  *half size only)`);
 let components = full.split("/");
-let h = 0;
+console.log(components);
 
 year = Number(components[0]);
 month = Number(components[1]);
 day = Number(components[2]);
 
-// console.log(year, month, day);
-
-C = Math.floor(year / 100);
-Y = year % 100;
-
-if(year <= 2) {
+if(month <= 2) {
   year -= 1;
   month += 12;
 }
 
-h = (day + Math.floor((26 * (month + 1)) / 10) + Y + Math.floor(Y / 4) + (-2 * C) + Math.floor(C / 4)) % 7;
+C = Math.floor(year / 100);
+Y = year % 100;
+
+h = (5 * C + Math.floor(C / 4) + Y + Math.floor(Y / 4) + Math.floor((26 * (month + 1)) / 10) + day + 6) % 7; 
+// day + parseInt((13 * (month + 1)) / 5) + Y + parseInt(Y / 4) + (-2 * C) + parseInt(C / 4);
+console.log(h);
 
 switch(h) { // リスト化してそれを使えばいいのかな、迷う
-  case 1:
+  case 0:
     document.getElementById("date").innerHTML = "日曜日(Sunday)";
     break;
-  case 2:
+  case 1:
     document.getElementById("date").innerHTML = "月曜日(Monday)";
     break;
-  case 3:
+  case 2:
     document.getElementById("date").innerHTML = "火曜日(Tuesday)";
     break;
-  case 4:
+  case 3:
     document.getElementById("date").innerHTML = "水曜日(Wednesday)";
     break;
-  case 5:
+  case 4:
     document.getElementById("date").innerHTML = "木曜日(Thursday)";
     break;
-  case 6:
+  case 5:
     document.getElementById("date").innerHTML = "金曜日(Friday)";
     break;
-  case 0:
+  case 6:
     document.getElementById("date").innerHTML = "土曜日(Saturday)";
     break;
   default:
